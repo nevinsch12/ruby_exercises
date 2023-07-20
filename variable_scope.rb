8751b286ce7efc2db0a9165eb1badba03919b737
nevinsch12 <christopher.nevins@icloud.com>
2023-07-19 21:23:31 -0500

    Variable Scope

---
+++ variable_scope.rb
@@ -0,0 +1,10 @@
+#1. 7.  Numbers in Ruby are immutable.
+#2. 7.  Not visible inside method definition.
+#3. 7.  Method definitions are self-contained (inpenetrable bubble).
+#4. Xy-zy.  Strings are mutable.  (This is a string method.)
+#5. This is reassignment.
+#6. Error.  Inpenetrable bubble.
+#7. 3.  last value of method invocation with block.
+#8. This is initialization, not reassignment.  Variable is not available in the right scope.
+#9. Shadowing by block parameter.
+#10. Undefined method.  (Not available because it is defined outside the method).

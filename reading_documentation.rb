bb821e4440b89ca1dce08be8fc646cf3a14abc20
nevinsch12 <christopher.nevins@icloud.com>
2023-07-19 20:41:13 -0500

    Reading Documentation 1

---
+++ reading_documentation.rb
@@ -0,0 +1,15 @@
+#1. ruby-doc.org
+
+#2. "control expressions"
+
+#3. nil unless break is used, in which case it returns the value of the argument
+
+#4. See above.
+
+#5. Use underscores.  Found under literals.
+
+#6. :Chris
+
+#7. Instance methods begin with #.  Class methods begin with ::
+
+#8. rjust
